02a23753b239a895f04ca92fbd63724651cd5715
unknown <priesjos000@warren.k12.in.us>
2019-01-24 13:43:28 -0500

    More math functions

--- Variables/lesson2.js
+++ Variables/lesson2.js
@@ -14,6 +14,26 @@ $(document).ready(function(){
         modulo();
     });
 
+    $("#time").click(function(){
+        time();
+    });
+
+    $("#sumDigits").click(function(){
+        sumDigits();
+    });
+
+    $("#pow").click(function(){
+        pow();
+    });
+
+    $("#abs").click(function(){
+        abs();
+    });
+
+    $("#sqrt").click(function(){
+        sqrt();
+    });
+
 });
 
 /*
@@ -31,6 +51,7 @@ var wrong = function(){
 */
 
 var add = function(){
+
     var x = $("#number1").val();
     var y = $("#number2").val();
 
@@ -69,5 +90,69 @@ var time = function(){
     //x will be converting from days to hours, mins, and seconds
     //y will be converting from seconds to days
 
+    var secondsRemaining;
+    var minutesRemaining;
+    var hoursRemaining;
+    var days;
+    var minutes;
+    var hours;
+
+    var input = $("#number2").val();
+
+    secondsRemaining = input%60;
+    minutes = Math.floor(input/60);
+    minutesRemaining = minutes%60;
+    hours = Math.floor(minutes/60);
+    hoursRemaining = hours%60;
+    days = Math.floor(hours/24);
+
+    $("#output").text(input + " seconds is equal to " + days + " days, " + hoursRemaining + " hours, " + minutesRemaining + " minutes, " + secondsRemaining + " seconds");
     
+}
+
+var sumDigits = function(){
+
+    //   512, pull 5, 1, and 2; mod 10 to find 2; divide 10 and mod 10 to find 1; divide 100 and mod 10 to find 5
+    var input = $("#number3").val();
+
+    var digit1 = input%10;
+    var digit2 = (Math.floor(input/10))%10;
+    var digit3 = (Math.floor(input/100))%10;
+
+    var sum = digit1 + digit2 + digit3;
+
+    $("#output2").text("Sum of the digits is " + sum);
+
+}
+
+var pow = function(){
+
+    //Math.pow(a, b) - raises a to the b power ||| Math.abs(a) - returns abs. value of a ||| Math.sqrt(a) - returns square root of a
+    //INCREMENT AND DECREMENT
+
+    var x = $("#number4").val();
+    var y = $("#number5").val();
+
+    var pow = Math.pow(x, y);
+
+    $("#output3").text("The product is " + pow);
+
+}
+
+var abs = function(){
+
+    var x = $("#number4").val();
+
+    var abs = Math.abs(x);
+    $("#output3").text("The absolute value is " + abs);
+
+}
+
+var sqrt = function(){
+
+    var x = $("#number4").val();
+
+    var sqrt = Math.sqrt(x);
+    $("#output3").text("The square root is " + sqrt);
+
 }
