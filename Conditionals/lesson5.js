37db734c63ba0ad9bed6f4907bd3e6e1ee81fd6b
unknown <priesjos000@warren.k12.in.us>
2019-02-07 13:38:19 -0500

    Studied conditional expressions

--- Conditionals/lesson5.js
+++ Conditionals/lesson5.js
@@ -1,19 +1,21 @@
 $(document).ready(function(){
 
-
     $("#comparisons").click(function(){
-        output("#input1","#input2");
+        output(compare("#input1","#input2"));
     });
 
     $("#conditional").click(function(){
-        conditional("#input1","#input2")
-        //output("#input1","#input2");
+
+        //output(conditional(input("#input1"), input("#input2")));
+        //output(evenOrOdd(input("#input1")));
+        //output(canDrive());
+        output(withLaw());
     })
 });
 
 var input = function(a){
 
-    var userInput = $(a).val();
+    var userInput = ($(a).val());
     return userInput;
 
 }
@@ -23,9 +25,9 @@ var compare = function(a,b){
     return input(a) > input(b);
 }
 
-var output = function(a,b){
+var output = function(a){
 
-    $("#output").text(conditional(input(a), input(b)));
+    $("#output").text(a);
 }
 
 /*
@@ -42,8 +44,48 @@ var output = function(a,b){
 
 var conditional = function(a,b){
 
-    $("#output").text("eee")
     if(a > b)
         return "A is bigger";
+    if(b > a)
+        return "B is bigger";
+    if(b == a)
+        return "They're equivalent";
+}
+
+var evenOrOdd = function(a){
+
+    if (a%2===0){
+        return(a + " is even");
+    }
+    else
+        return(a + " is odd");
     
+}
+
+//Compound boolean expressions use AND (&&), OR (||), NOR, to make even deeper logical conjunctions.
+//If you are 16 and have a license, you can legally drive.
+
+var canDrive = function(){
+
+    var age = parseInt(input("#input1"));
+    var license = input("#input2");
+
+    if(age >= 16 && license == "yes"){
+        return "You can drive";
+    }
+    else
+        return "You should not drive";
+
+}
+
+var withLaw = function(){
+
+    var AP = (input("#input1"));
+    var CS3 = (input("#input2"));
+
+    if(AP === "yes" || CS3 == "yes"){
+        return "You will be in Law's class.";
+    }
+    else   
+        return "You will not be in Law's class."
 }
