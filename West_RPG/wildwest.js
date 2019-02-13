a233a8e36cb9dea512fbb653b5428a6c2ec541b1
unknown <priesjos000@warren.k12.in.us>
2019-02-13 13:43:55 -0500

    Finished

--- West_RPG/wildwest.js
+++ West_RPG/wildwest.js
@@ -1,52 +1,91 @@
 $(document).ready(function(){
 
-    var N2B1 = createButton("N2O1", N2O1);
-    var N2B2 = createButton("N2O2", N2O2);
-    var N2B3 = createButton("N2O3", N2O3);
-    var N3B1 = createButton("N3O1", N3O1);
-    var N3B2 = createButton("N3O2", N3O2);
-    var N5B1 = createButton("N5O1", N5O1);
-    var N6B1 = createButton("N6O1", N6O1);
-
-
     updateNarrative(N1);
 
-    //Choose ranger or robot
-    $("footer").html(createButton("N1O1", N1O1) + createButton("N1O2", N1O2));
+    $("footer").html(createButton("B1", N1O1) + createButton("B2", N1O2) + createButton("B3", "unused"));
+    $("#B3").hide()
 
-    //Ranger selected
-    $("#N1O1").click(function(){
-        updateNarrative(N2);
-        $("footer").html(N2B1 + N2B2 + N2B3);
-        hasBow = true;
-    })
+    $("#B1").click(function(){
+        if($(this).text() === N1O1){
+            updateNarrative(N2);
+            $("#B1").text(N2O1);
+            $("#B2").text(N2O2);
+            $("#B3").text(N2O3);
+            $("#B3").show();
+        }
 
-    //Robot selected
-    $("#N1O2").click(function(){
-        updateNarrative(N3);
-        $("footer").html(createButton("N3O1", N3B1) + createButton("N3O2", N3B2));
-        hasPhaser = true;
-    })
+        else if ($("#B1").text() === N2O1){
+            updateNarrative(N5);
+            $("#B1").text(N5O1);
+            $("#B2").hide();
+            $("#B3").hide();
+        }
 
-    var roleButton;
-    if(hasBow){
-        roleButton = createButton("N5O1", N5B1);
-        
-    }
-    else
-        roleButton = createButton("N6O1", N6B1);
+        else if ($("#B1").text() === N5O1){
+            updateNarrative(N8);
+            $("#B1").text("End");
+            $("#B2").hide();
+            $("#B3").hide();
+        }
 
-    //Chase tumbleweeds selected
-    $("#N2O1").click(function(){
-        updateNarrative(N5);
-        $("footer").html(roleButton);
-    })
+        else if ($("#B1").text() === N6O1){
+            updateNarrative(N9);
+            $("#B1").text("End");
+            $("#B2").hide();
+            $("#B3").hide();
+        }
 
-    //Purge wild west of evil-doers selected
-    $("#N3O2").click(function(){
-        updateNarrative(N6);
-        $("footer").html(roleButton);
-    })
+        else if($("#B1").text() === N7O1){
+            updateNarrative(N10);
+            $("#B1").text("End");
+            $("#B2").hide();
+            $("#B3").hide();
+        }
+    });
+
+    $("#B2").click(function(){
+        if($(this).text() === N1O2){
+            updateNarrative(N3);
+            $("#B1").text(N3O1);
+            $("#B2").text(N3O2);
+            $("#B3").hide();
+        }
+        else if ($("#B2").text() === N2O2){
+            updateNarrative(N4);
+            $("#B1").text("End");
+            $("#B2").hide();
+            $("#B3").hide();
+        }
+        else if ($("#B2").text() === N3O2){
+            updateNarrative(N6);
+            $("#B1").text(N6O1);
+            $("#B2").hide();
+            $("#B3").hide();
+        }
+
+        else if($("#B2").text() === N7O2){
+            updateNarrative(N10);
+            $("#B1").text("End");
+            $("#B2").hide();
+            $("#B3").hide();
+        }
+    });
+
+    $("#B3").click(function(){
+        if($("#B3").text() === N2O3){
+            updateNarrative(N7);
+            $("#B1").text(N7O1);
+            $("#B2").text(N7O2);
+            $("#B3").text(N7O3);
+        }
+
+        else if($("#B3").text() === N7O3){
+            updateNarrative(N10);
+            $("#B1").text("End");
+            $("#B2").hide();
+            $("#B3").hide();
+        }
+    });
 })
 
 var hasBow = false;
@@ -63,15 +102,26 @@ var N2O3 = "arrest bad robots.";
 
 var N3 = "This robot had killer instincts - to purge the Wild West of all evil-doers. Armed with a phaser, he ...";
 var N3O1 = "heats his pork and beans.";
-var N3O2 = "purges the Wild West of evil-doers."
+var N3O2 = "purges the Wild West of evil-doers.";
 
-var N4
+var N4 = "He continued to cook pancakes for the rest of his days and died a very uneventful life.";
 
-var N5 = "The ranger approaches the insiduous Mr. Pig. He raises his bow and ..."
-var N5O1 = "shoots the arrow through Mr. Pig."
+var N5 = "The ranger approaches the insiduous Mr. Pig. He raises his bow and ...";
+var N5O1 = "shoots the arrow through Mr. Pig.";
 
-var N6 = "The robot approaches the insiduous Mr. Pig. He raises his phaser and ..."
-var N6O1 = "fires it, vaporizing Mr. Pig."
+var N6 = "The robot approaches the insiduous Mr. Pig. He raises his phaser and ...";
+var N6O1 = "fires it, vaporizing Mr. Pig.";
+
+var N7 = "After a while, the ranger comes across a rogue robot armed with a phaser. In response, he ...";
+var N7O1 = "attempts to pacify the robot diplomatically.";
+var N7O2 = "aims his bow at a critical component of the robot, firing.";
+var N7O3 = "tries to sneak toward the robot.";
+
+var N8 = "Mr. Pig was fatally struck by the arrow, and let out a long screech before collapsing."
+
+var N9 = "Mr. Pig was incinerated to nothing more than ashes and smoke."
+
+var N10 = "The ranger was no match for the robot's superior technology and was incinerated."
 
 var updateNarrative = function(a){
 
