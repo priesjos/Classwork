c5c5464592a0035dacced2e89b39ebe99860e331
unknown <priesjos000@warren.k12.in.us>
2019-04-08 13:05:21 -0400

    yes

--- Cards/cards.js
+++ Cards/cards.js
@@ -50,9 +50,39 @@ function cardToString(){
 }
 
 function cardImage(){
-    var img = document.createElement("IMG");
-    img.src = "cards_png_zip/PNG/" + this.rank + this.suit + ".png";
-    return img
+
+    var rank, suit;
+
+    switch(this.rank){
+
+        case 1: rank = "A"; break;
+        case 2: rank = "2"; break;
+        case 3: rank = "3"; break;
+        case 4: rank = "4"; break;
+        case 5: rank = "5"; break;
+        case 6: rank = "6"; break;
+        case 7: rank = "7"; break;
+        case 8: rank = "8"; break;
+        case 9: rank = "9"; break;
+        case 10: rank = "10"; break;
+        case 11: rank = "J"; break;
+        case 12: rank = "Q"; break;
+        case 13: rank = "K"; break;
+        default: rank = null; break;
+
+    }
+
+    switch(this.suit){
+
+        case 1: suit = "C"; break;
+        case 2: suit = "D"; break;
+        case 3: suit = "H"; break;
+        case 4: suit = "S"; break;
+        default: suit = null; break;
+
+    }
+
+    return "<img src='/cards'" + rank  + suit + ".png' />";
 }
 
 var rank = parseInt((Math.random()*12)+1);
