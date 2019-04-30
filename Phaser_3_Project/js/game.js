bdbc4b04714acb62b4753ca984abe388399cdfb2
unknown <priesjos000@warren.k12.in.us>
2019-04-30 13:35:06 -0400

    following tutorial for better organization

---
+++ Phaser_3_Project/js/game.js
@@ -0,0 +1,21 @@
+var config = {
+    type: Phaser.WebGL,
+    width: 480,
+    height: 640,
+    backgroundColor: "black",
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { x: 0, y: 0 }
+        }
+    },
+    scene: [
+        SceneMainMenu,
+        SceneMain,
+        SceneGameOver
+    ],
+    pixelArt: true,
+    roundPixels: true
+}
+
+var game = new Phaser.Game(config);
